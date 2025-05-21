7434b42a579d35d65fe99eebe8d4614ece5a97c3
AlvesKZ <igoralvex01@gmail.com>
2025-05-21 14:54:01 -0300

    first commit

---
+++ eslint.config.mjs
@@ -0,0 +1,33 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    languageOptions: {
+      ecmaVersion: 2021,
+      globals: {
+        ...globals.node,
+      },
+    },
+    plugins: {
+      js,
+    },
+    extends: ['js/recommended'],
+    rules: {
+      quotes: ['warn', 'single'],
+      semi: ['warn', 'always'],
+      indent: ['warn', 2],
+      'comma-dangle': ['warn', 'always-multiline'],
+      'space-before-function-paren': ['warn', 'never'],
+      'no-console': 'off',
+      'class-methods-use-this': 'off',
+    },
+  },
+]);
+
+
+
+2/2;
+
