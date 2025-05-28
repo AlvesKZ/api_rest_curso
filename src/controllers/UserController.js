94ed89294c64cfa34093c4396550f494241cb7da
AlvesKZ <igoralvex01@gmail.com>
2025-05-28 10:27:45 -0300

    Correções de segurança

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -4,21 +4,20 @@ class UserController {
   async store(req, res) {
     try {
       const novoUser = await User.create(req.body);
-
-      return res.json(novoUser);
-    } catch(e) {
+      const { id, nome, email } = novoUser;
+      return res.json({ id, nome, email });
+    } catch (e) {
       return res.status(400).json({
-        errors: e.errors.map(err => err.message),
+        errors: e.errors.map((err) => err.message),
       });
     }
-
   }
 
   async index(req, res) {
     try {
-      const users = await User.findAll();
+      const users = await User.findAll({ attributes: ["id", "nome", "email"] });
       return res.json(users);
-    }catch(e) {
+    } catch (e) {
       return res.json(null);
     }
   }
@@ -26,31 +25,28 @@ class UserController {
   async show(req, res) {
     try {
       const user = await User.findByPk(req.params.id);
-      return res.json(user);
-    }catch(e) {
+
+      const { id, nome, email } = user;
+      return res.json({ id, nome, email });
+    } catch (e) {
       return res.json(null);
     }
   }
 
   async update(req, res) {
     try {
-      if(!req.params.id){
-        return res.status(400).json({
-          errors: ["ID não enviado"],
-        });
-      }
+      const user = await User.findByPk(req.userId);
 
-      const user = await User.findByPk(req.params.id);
-
-      if(!user){
+      if (!user) {
         return res.status(400).json({
           errors: ["Usuário não existe"],
         });
       }
 
       const novosDados = await user.update(req.body);
-      return res.json(novosDados);
-    }catch(e) {
+      const { id, nome, email } = novosDados;
+      return res.json({ id, nome, email });
+    } catch (e) {
       return res.status(400).json({
         errors: e.errors.map((err) => err.message),
       });
@@ -59,27 +55,22 @@ class UserController {
 
   async delete(req, res) {
     try {
-      if(!req.params.id){
-        return res.status(400).json({
-          errors: ["ID não enviado"],
-        });
-      }
+      const user = await User.findByPk(req.userId);
 
-      const user = await User.findByPk(req.params.id);
-
-      if(!user){
+      if (!user) {
         return res.status(400).json({
           errors: ["Usuário não existe"],
         });
       }
 
       await user.destroy();
-      return res.json(user);
-    }catch(e) {
+      return res.json(null);
+    } catch (e) {
       return res.status(400).json({
         errors: e.errors.map((err) => err.message),
       });
     }
   }
 }
+
 export default new UserController();
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,20 +1,24 @@
 import { Router } from "express";
 import userController from "../controllers/UserController.js";
 
+import loginRequired from "../middlewares/loginRequired.js";
+
 const router = new Router();
 
+// Não deveria existir
+// router.get('/', userController.index); // Lista usuários
+// router.get('/:id', userController.show); // Lista usuário
+
 router.post("/", userController.store);
-router.get("/", userController.index);
-router.get("/:id", userController.show);
-router.put("/:id", userController.update);
-router.delete("/:id", userController.delete);
+router.put("/", loginRequired, userController.update);
+router.delete("/", loginRequired, userController.delete);
 
 export default router;
 
 /*
-index -> lista todos o usuários -> GET
+index -> lista todos os usuários -> GET
 store/create -> cria um novo usuário -> POST
 delete -> apaga um usuário -> DELETE
 show -> mostra um usuário -> GET
-update -> altera um usuário -> PATCH ou PUT
+update -> atualiza um usuário -> PATCH ou PUT
 */
