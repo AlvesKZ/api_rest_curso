82ac3239c61749c5fe6f94896f6b73c995f7de2f
AlvesKZ <igoralvex01@gmail.com>
2025-05-30 20:26:21 -0300

    Cria upload de arquivos

---
+++ src/config/multerConfig.js
@@ -0,0 +1,19 @@
+import { fileURLToPath } from "url";
+import { dirname, extname, resolve } from "path";
+import multer from "multer";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const aleatorio = () => Math.floor(Math.random() * 10000 + 10000);
+
+export default {
+  storage: multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, resolve(__dirname, "..", "..", "uploads"));
+    },
+    filename: (req, file, cb) => {
+      cb(null, `${Date.now()}_${aleatorio()}${extname(file.originalname)}`);
+    },
+  }),
+};
