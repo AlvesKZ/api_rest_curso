fa1317dca2bf980709adca4ac5095714ce14c028
AlvesKZ <igoralvex01@gmail.com>
2025-05-30 19:53:10 -0300

    Cria CRUD de alunos

--- src/routes/alunoRoutes.js
+++ src/routes/alunoRoutes.js
@@ -1,8 +1,14 @@
 import { Router } from "express";
 import alunoController from "../controllers/AlunoController.js";
 
+import loginRequred from "../middlewares/loginRequired.js";
+
 const router = new Router();
 
 router.get("/", alunoController.index);
+router.post("/", loginRequred, alunoController.store);
+router.put("/:id", loginRequred, alunoController.update);
+router.get("/:id",  alunoController.show);
+router.delete("/:id", loginRequred, alunoController.delete);
 
 export default router;
