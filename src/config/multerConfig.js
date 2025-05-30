172ba2c1e7e5626600d3d442f07edd86b4c249b7
AlvesKZ <igoralvex01@gmail.com>
2025-05-30 20:43:32 -0300

    Valida arquivos enviados

--- src/config/multerConfig.js
+++ src/config/multerConfig.js
@@ -8,6 +8,13 @@ const __dirname = dirname(__filename);
 const aleatorio = () => Math.floor(Math.random() * 10000 + 10000);
 
 export default {
+  fileFilter: (req, file, cb) => {
+    if(file.mimetype !== "image/png" && file.mimetype !== "image/jpeg"){
+      return cb(new multer.MulterError("Arquivo precise ser PNG ou JPG."));
+    }
+
+    return cb(null, true);
+  },
   storage: multer.diskStorage({
     destination: (req, file, cb) => {
       cb(null, resolve(__dirname, "..", "..", "uploads"));
