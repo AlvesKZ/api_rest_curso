e88279adcb36168fa3c3245ff98f94d8d68b83fd
AlvesKZ <igoralvex01@gmail.com>
2025-06-02 21:59:04 -0300

    Listando fotos com os alunos

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -5,20 +5,11 @@ import loginRequired from "../middlewares/loginRequired.js";
 
 const router = new Router();
 
-// Não deveria existir
-// router.get("/", userController.index); // Lista usuários
-// router.get("/:id", userController.show); // Lista usuário
+// router.get("/", userController.index);
+// router.get("/:id", userController.show);
 
-router.post("/", userController.store);
+router.post("/", loginRequired, userController.store);
 router.put("/", loginRequired, userController.update);
 router.delete("/", loginRequired, userController.delete);
 
 export default router;
-
-/*
-index -> lista todos os usuários -> GET
-store/create -> cria um novo usuário -> POST
-delete -> apaga um usuário -> DELETE
-show -> mostra um usuário -> GET
-update -> atualiza um usuário -> PATCH ou PUT
-*/
